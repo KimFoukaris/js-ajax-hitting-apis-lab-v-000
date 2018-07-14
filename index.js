dd1c0b5e2e07d5693fbe5bdac670c0cfbd568162
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-14 14:01:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function displayRepositories(event, data) {
 }
 
 function getCommits(el) {
-  const name = el.dataset.repo
+  const name = el.dataset.repository
   console.log(name)
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits)
